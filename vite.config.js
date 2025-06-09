34638a1dbcaa26a36f5a6de3bb0570a29b7f5ed1
Vinicius chiabai <Vinicius.chiabai@gmail.com>
2025-06-09 01:25:19 -0300

    update no vite.config para o githubPages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cesta/', // <-- ADICIONE OU MODIFIQUE ESTA LINHA!
+  base: '/VendadeCestas/', // <-- MODIFICADO PARA O NOVO NOME DO REPOSITÓRIO!
 })
